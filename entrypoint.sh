e19e31158dd9be2fc36581e27136cfec1835756f
Lars Jacobsson <lars.jacobsson@mathem.se>
2023-01-10 21:11:48 +0100

    fixing url

--- entrypoint.sh
+++ entrypoint.sh
@@ -16,9 +16,9 @@ if [ $err -eq 1 ]; then
 fi
 
 # Install Hugo
-HUGO_VERSION=$(curl -s https://api.github.com/repos/gohugoio/hugo/releases/latest | jq -r '.tag_name')
-mkdir tmp/ && cd tmp/
-curl -sSL https://github.com/gohugoio/hugo/releases/download/${HUGO_VERSION}/hugo_extended_${HUGO_VERSION: -6}_Linux-64bit.tar.gz | tar -xvzf-
+# HUGO_VERSION=$(curl -s https://api.github.com/repos/gohugoio/hugo/releases/latest | jq -r '.tag_name')
+mkdir tmp/ && cd tmp/          
+curl -sSL https://github.com/gohugoio/hugo/releases/download/v0.109.0/hugo_extended_0.109.0_Linux-64bit.tar.gz | tar -xvzf-
 mv hugo /usr/local/bin/
 cd .. && rm -rf tmp/
 cd ${GITHUB_WORKSPACE}
